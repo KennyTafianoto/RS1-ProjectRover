9570492ef5684161c1dd3adea439b64249c0a2bf
Lucas Moore <lucas.j.moore@student.uts.edu.au>
2023-09-10 20:31:12 +1000

    Added amcl data to graph

--- rs_filters/filter_analysis/analysis.py
+++ rs_filters/filter_analysis/analysis.py
@@ -7,8 +7,12 @@ EKF_data_location = np.genfromtxt('data/EKF_data.csv', delimiter=',', skip_heade
 AMCL_data_location = np.genfromtxt('data/AMCL_data.csv', delimiter=',', skip_header=1)
 
 # Ground truth (x, y) positions
-ground_truth_x = EKF_data_location[:,0]
-ground_truth_y = EKF_data_location[:,1]
+ground_truth_x_EKF = EKF_data_location[:,0]
+ground_truth_y_EKF = EKF_data_location[:,1]
+
+# Just testing that they're starting at the same time
+ground_truth_x_AMCL = AMCL_data_location[:,0]
+ground_truth_y_AMCL = AMCL_data_location[:,1]
 
 # Localisation estimates
 EKF_localisation_x = EKF_data_location[:,6]
@@ -21,23 +25,26 @@ AMCL_localisation_y = AMCL_data_location[:,7]
 def calculate_rmse(ground_truth, estimate):
     return np.sqrt(np.mean((ground_truth - estimate)**2))
 
-# Calculate RMSE for both x and y coordinates
-rmse_x = calculate_rmse(ground_truth_x, EKF_localisation_x)
-rmse_y = calculate_rmse(ground_truth_y, EKF_localisation_y)
-combined_rmse = np.sqrt(rmse_x**2 + rmse_y**2)
+# Calculate RMSE for EKF
+EKF_rmse_x = calculate_rmse(ground_truth_x_EKF, EKF_localisation_x)
+EKF_rmse_y = calculate_rmse(ground_truth_y_EKF, EKF_localisation_y)
+EKF_combined_rmse = np.sqrt(EKF_rmse_x**2 + EKF_rmse_x**2)
+
+# Calculate RMSE for AMCL
+AMCL_rmse_x = calculate_rmse(ground_truth_x_AMCL, AMCL_localisation_x)
+AMCL_rmse_y = calculate_rmse(ground_truth_y_AMCL, AMCL_localisation_y)
+AMCL_combined_rmse = np.sqrt(AMCL_rmse_x**2 + AMCL_rmse_y**2)
 
 # Graphing
 plt.figure(figsize=(12, 8))
-plt.plot(ground_truth_x, ground_truth_y, label='Ground Truth', color='black', alpha=1.0, linestyle='-', linewidth=1)
+#plt.plot(ground_truth_x_2, ground_truth_y_2, label='Ground Truth 2', color='r', alpha=1.0, linestyle='-', linewidth=1, marker='x')
+plt.plot(ground_truth_x_EKF, ground_truth_y_EKF, label='Ground Truth', color='black', alpha=1, linestyle='-', linewidth=1)
 plt.plot(EKF_localisation_x, EKF_localisation_y, label='EKF Estimate', color='r', alpha=0.6, linestyle='-', linewidth=1)
-
 plt.plot(AMCL_localisation_x, AMCL_localisation_y, label='AMCL Estimate', color='g', alpha=0.6, linestyle='-', linewidth=1)
 
-
 # Adding annotations for RMSE
-plt.text(1.1, 0.6, f'RMSE (X): {rmse_x:.3f}', transform=plt.gca().transAxes, fontsize=12, ha='left', va='center', color='black')
-plt.text(1.1, 0.5, f'RMSE (Y): {rmse_y:.3f}', transform=plt.gca().transAxes, fontsize=12, ha='left', va='center', color='black')
-plt.text(1.1, 0.4, f'RMSE (combined): {combined_rmse:.3f}', transform=plt.gca().transAxes, fontsize=12, ha='left', va='center', color='black')
+plt.text(1.1, 0.52, f'RMSE EKF (combined): {EKF_combined_rmse:.3f}', transform=plt.gca().transAxes, fontsize=12, ha='left', va='center', color='black')
+plt.text(1.1, 0.48, f'RMSE AMCL (combined): {AMCL_combined_rmse:.3f}', transform=plt.gca().transAxes, fontsize=12, ha='left', va='center', color='black')
 plt.tight_layout(rect=[0.1, 0.1, 0.9, 0.9])
 
 plt.title('Ground Truth vs Localisation Estimates', fontsize=16)
@@ -47,8 +54,8 @@ plt.legend(loc='upper left', fontsize=12)
 plt.grid(True, linestyle='--', linewidth=0.5, color='gray')
 
 # Add axis limits for better visualization
-plt.xlim([0, (max(ground_truth_x)+0.1)])
-plt.ylim([0, (max(ground_truth_y)+0.1)])
+plt.xlim([0, (max(ground_truth_x_EKF)+0.1)])
+plt.ylim([0, (max(ground_truth_y_EKF)+0.1)])
 
 # Show the plot
 plt.show()
