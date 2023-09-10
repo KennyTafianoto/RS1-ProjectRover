c28b6ece3651377cb5cad40ede9de69941473542
Lucas Moore <lucas.j.moore@student.uts.edu.au>
2023-09-10 12:29:00 +1000

    seperating into 3 different executables

--- rs_odom_noise/src/noise_and_record.cpp
+++ rs_odom_noise/src/noise_and_record.cpp
@@ -14,27 +14,79 @@
 #include <tf2_geometry_msgs/tf2_geometry_msgs.h>
 
 // Global Variables
+std::string logFilePath = "odometry_data.csv";
 std::string filtered_node_name_ = "/robot_pose_ekf/odom_combined";
+std::float_t samplePeriod_ = 0.1; // time period between each sample saved to the csv file
 ros::Publisher noisy_odom_publisher;
+ros::Subscriber filtered_odom_subscriber;
+std::ofstream csv_file;
 std::stringstream filteredData_, rawOdomData_, NoisyOdomData_;
 
 // Function Prototypes
+bool checkFilteredTopicAvailability();
 double getRandomNoise();
+void FilteredOdomCallback(const geometry_msgs::PoseWithCovarianceStamped::ConstPtr &filtered_odom_msg);
 void odomCallback(const nav_msgs::Odometry::ConstPtr &odom_msg);
+void writeDataToCSV(const ros::TimerEvent &);
 
 int main(int argc, char **argv)
 {
     // ROS Initialization
     ros::init(argc, argv, "noisy_odometry_publisher");
     ros::NodeHandle nh;
-    
+
+    // Open CSV File
+    csv_file.open(logFilePath);
+    if (!csv_file.is_open())
+    {
+        ROS_ERROR("Failed to open the CSV file for writing.");
+        return 1;
+    }
+    csv_file << "Original_X,Original_Y,Original_Z,Noisy_X,Noisy_Y,Noisy_Z,Filtered_X,Filtered_Y,Filtered_Z\n";
+
+    // Check Topic Availability and Subscribe
+    if (checkFilteredTopicAvailability())
+    {
+        filtered_odom_subscriber = nh.subscribe(filtered_node_name_, 10, FilteredOdomCallback);
+    }
+
     // Subscribe to Odometry and Advertise Noisy Odometry
     ros::Subscriber odom_subscriber = nh.subscribe("/odom", 10, odomCallback);
     noisy_odom_publisher = nh.advertise<nav_msgs::Odometry>("/noisy_odom", 10);
 
+    // Timer for Writing Data to CSV
+    ros::Timer timer = nh.createTimer(ros::Duration(samplePeriod_), writeDataToCSV);
+
     // ROS Spin
     ros::spin();
     return 0;
+}
+
+bool checkFilteredTopicAvailability()
+{
+    std::vector<ros::master::TopicInfo> topic_infos;
+    bool found_ = false;
+    if (ros::master::getTopics(topic_infos))
+    {
+        for (const auto &info : topic_infos)
+        {
+            if (info.name == filtered_node_name_)
+            {
+                found_ = true;
+                break;
+            }
+        }
+        if (!found_)
+        {
+            ROS_INFO("Topic /odom_filtered not found, writing to file anyway...");
+            filteredData_ << "-"
+                          << ","
+                          << "-"
+                          << ","
+                          << "-";
+        }
+    }
+    return found_;
 }
 
 double getRandomNoise()
@@ -43,6 +95,15 @@ double getRandomNoise()
     std::mt19937 gen(rd());
     std::uniform_real_distribution<double> dis(-0.05, 0.05);
     return dis(gen);
+}
+
+void FilteredOdomCallback(const geometry_msgs::PoseWithCovarianceStamped::ConstPtr &filtered_odom_msg)
+{
+    filteredData_.str("");
+    filteredData_.clear();
+    filteredData_ << filtered_odom_msg->pose.pose.position.x << ","
+                  << filtered_odom_msg->pose.pose.position.y << ","
+                  << filtered_odom_msg->pose.pose.position.z;
 }
 
 void odomCallback(const nav_msgs::Odometry::ConstPtr &odom_msg)
@@ -90,4 +151,26 @@ void odomCallback(const nav_msgs::Odometry::ConstPtr &odom_msg)
     noisy_odom.pose.covariance[35] = variance_yaw;   // Variance in yaw
 
     noisy_odom_publisher.publish(noisy_odom);
+
+    rawOdomData_.str("");
+    rawOdomData_.clear();
+    NoisyOdomData_.str("");
+    NoisyOdomData_.clear();
+
+    rawOdomData_ << odom_msg->pose.pose.position.x << ","
+                 << odom_msg->pose.pose.position.y << ","
+                 << odom_msg->pose.pose.position.z;
+
+    NoisyOdomData_ << noisy_odom.pose.pose.position.x << ","
+                   << noisy_odom.pose.pose.position.y << ","
+                   << noisy_odom.pose.pose.position.z;
+}
+
+void writeDataToCSV(const ros::TimerEvent &)
+{
+    if (!rawOdomData_.str().empty() && !NoisyOdomData_.str().empty() && !filteredData_.str().empty())
+    {
+        csv_file << rawOdomData_.str() << "," << NoisyOdomData_.str() << "," << filteredData_.str() << "\n";
+        csv_file.flush();
+    }
 }
